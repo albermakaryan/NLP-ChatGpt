a9aa1069394858f682a424ff3111c1e23bdd52c8
Alber <makaryanalber@gmail.com>
2024-01-10 14:21:01 +0400

    world magazine articles

--- SRC/main.py
+++ SRC/main.py
@@ -4,4 +4,23 @@ import os
 
 model = torch.load("../models/gpt.pt")
 
-print(model.parameters)
+from gpt import GPTLanguageModel
+
+from gpt import decode,encode
+
+
+while True:
+    
+    context = input("Enter a prompt: ")
+    
+    if context == "exit":
+        break
+    
+    context = encode(context)
+    
+    response = model.generate(context)
+    
+    print(decode(response))
+    
+    
+    
